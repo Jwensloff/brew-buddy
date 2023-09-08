7f3edf3e3201d7629acf4e927fdb39a06a4fa731
Josh Bennett <joshbennett793@yahoo.com>
2023-09-07 22:48:04 -0500

    Initial setup of fetch call

---
+++ src/apiCalls.js
@@ -0,0 +1,16 @@
+export default async function getBreweries(city) {
+  // 50 results is the default 'per_page' param
+  // You must indicate whether you want to return more
+  city = city.split(' ').reduce((acc, curr) => acc + `_${curr}`);
+  try {
+    const response = await fetch(
+      `https://api.openbrewerydb.org/v1/breweries?by_city=${city}`
+    );
+    if (!response.ok) {
+      throw new Error('Custom error for now');
+    }
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
