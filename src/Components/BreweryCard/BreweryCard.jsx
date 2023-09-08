84b6fbc0c612623212b9ef896e314dec1c3372e3
Renee Pinna <renee.pinna@gmail.com>
2023-09-07 20:01:35 -0600

    Add breweryCar styling

--- src/Components/BreweryCard/BreweryCard.jsx
+++ src/Components/BreweryCard/BreweryCard.jsx
@@ -1,7 +1,15 @@
 import './BreweryCard.scss';
 
 function BreweryCard() {
-  return <div>Card</div>;
+  return <article className='breweryCard'>
+    <p className='card-text name'>Name</p>
+    <p className='card-text'>Address</p>
+    <p className='card-text'>phone</p>
+    <p className='card-text'> type</p>
+    <a className='card-a'>Website</a>
+    <a className='card-a'>Directions</a>
+    <button className='card-btn'>fav</button>
+  </article>;
 }
 
 export default BreweryCard;
