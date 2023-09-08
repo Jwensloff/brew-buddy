e05ab17eb69d6a5a3ba1d990c3afa7a24c8aadc1
Jocelyn Wensloff <jewensloff@gmail.com>
2023-09-07 20:34:29 -0600

    clean up

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,53 +1,20 @@
 import './Map.scss';
-// import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from 'react-leaflet';
 
-import { MapContainer, TileLayer } from "react-leaflet";
- 
 function Map() {
   const position = [39.82, -98.57]; // [latitude, longitude]
   const zoomLevel = 4;
- 
+
   return (
-        <div className="map__container">
-
-    <MapContainer 
-        center={position} 
-        zoom={zoomLevel} 
-        scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
-    </MapContainer>
-        </div>
-
+    <div className='map__container'>
+      <MapContainer center={position} zoom={zoomLevel} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+      </MapContainer>
+    </div>
   );
-};
+}
 
 export default Map;
-
-
-
-
-
-
-
-// function Map() {
-//   const defaultPosition: LatLngExpression = [48.864716, 2.349]; // Paris position
-
-//   return (
-//     <div className="map__container">
-//       <MapContainer
-//         center={defaultPosition}
-//         zoom={13}
-//       >
-//         <TileLayer
-//           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//         />
-//       </MapContainer>
-//     </div>
-//   );}
-
-// export default Map;
