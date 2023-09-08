408411b78521e204381736c8dbb023fb54f37d55
Josh Bennett <joshbennett793@yahoo.com>
2023-09-07 22:46:56 -0500

    Set up state and pass as prop to Homepaget

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,17 +2,24 @@ import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Homepage from '../Homepage/Homepage';
 import Search from '../Search/Search';
-
+import { useState } from 'react';
 
 function App() {
+  const [breweries, setBreweries] = useState([]);
+
   return (
-    <div className="App">
+    <div className='App'>
       <header className='mainHeader'>
-        <Search/>
+        <Search />
       </header>
 
       <Routes>
-        <Route path='/' element={<Homepage/>}/>
+        <Route
+          path='/'
+          element={
+            <Homepage setBreweries={setBreweries} breweries={breweries} />
+          }
+        />
       </Routes>
     </div>
   );
