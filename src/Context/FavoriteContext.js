6dcfb1e7dfa738abc261eb326c105f2b4128399e
Renee Pinna <renee.pinna@gmail.com>
2023-09-09 11:18:17 -0600

    Add favorite context

---
+++ src/Context/FavoriteContext.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from 'react';
+
+export const FavoriteContext = createContext(null);
+
+export function FavoriteContextProvider({ children }) {
+  const [favorites, setFavorites] = useState([]);
+
+  function toggleFavorite(brewery) {
+    if (favorites.includes(brewery)) {
+      setFavorites(
+        favorites.filter(favBrewery => favBrewery.id !== brewery.id),
+      );
+    } else {
+      setFavorites([...favorites, brewery]);
+    }
+  }
+
+  const value = {
+    favorites,
+    toggleFavorite,
+  };
+
+  return (
+    <FavoriteContext.Provider value={value}>
+      {children}
+    </FavoriteContext.Provider>
+  );
+}
+
+export function useFavorites() {
+  const favorites = useContext(FavoriteContext);
+
+  if (!favorites) {
+    throw new Error('useFavorites must be used within FavoriteContextProvider');
+  }
+
+  return favorites;
+}
