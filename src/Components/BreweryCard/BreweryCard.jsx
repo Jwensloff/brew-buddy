477ba31b7cc9d8c4cec6c8c0e58c915881a5422d
Renee Pinna <renee.pinna@gmail.com>
2023-09-09 13:40:42 -0600

    Add favorites trackiing to brewery cards

--- src/Components/BreweryCard/BreweryCard.jsx
+++ src/Components/BreweryCard/BreweryCard.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useFavorites } from '../../Context/FavoriteContext';
 import './BreweryCard.scss';
 
 function BreweryCard({ brewery }) {
   const { name, street, phone, brewery_type, website_url, city } = brewery;
   const directionsURL = `https://www.google.com/maps/dir/${name},${street}+${city}`;
+  const { toggleFavorite, favorites } = useFavorites();
+  const [isFavorite, setIsFavorite] = useState(false);
+  let className;
+
+  function toggleIsFavorite() {
+    if (isFavorite) {
+      setIsFavorite(false);
+    } else {
+      setIsFavorite(true);
+    }
+  }
+
+  const favoriteStyles = {
+    backgroundColor: isFavorite ? '#000' : '#fff',
+  };
 
   return (
     <article className='breweryCard'>
@@ -21,7 +38,15 @@ function BreweryCard({ brewery }) {
       >
         Directions
       </a>
-      <button className='card-btn'>fav</button>
+      <button
+        style={favoriteStyles}
+        onClick={() => {
+          toggleFavorite(brewery);
+          toggleIsFavorite();
+        }}
+      >
+        fav
+      </button>
     </article>
   );
 }
