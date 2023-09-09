ec05a2c1e9a4251a21046331a1ea4426ee9b09b9
Banjamin Hatch <banjaminhatch@gmail.com>
2023-09-08 21:37:56 -0600

    Implement map re-rendering with center and zoom

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,42 +1,41 @@
 import './Map.scss';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useBreweries } from '../../Context/BreweryContext';
+import {useEffect, useState} from 'react'
 
 function Map() {
-  const position = [39.82, -98.57]; // [latitude, longitude]
-  const zoomLevel = 10;
+  const position = [39.82, -98.57];
+  const zoomLevel = 5;
   const {breweries} = useBreweries();
-  console.log("Breweries:",breweries)
-
+  const [validBreweries, setValidBreweries] = useState([])
+  const [mapKey, setMapKey] = useState(0)
   
   
+  useEffect(() =>{
+  const filteredBreweries = breweries.filter(brewery => brewery.latitude && brewery.longitude)
+    setValidBreweries(filteredBreweries)
+    setMapKey(prevKey => prevKey + 1)
+  },[breweries])
 
-  const breweryPins = breweries.map((brewery) => {
-    console.log('LAT:',brewery.latitude)
-    console.log("LonG:", brewery.longitude)
-    if(brewery.latitude && brewery.longitude){
-      console.log("TEST")
-    return <Marker key={brewery.id} position={[brewery.latitude, brewery.longitude]}>
+
+  const mapPoints = validBreweries.map((brewery) => {
+    return (
+      <Marker key={brewery.id} position={[brewery.latitude, brewery.longitude]}>
       <Popup>{brewery.name}</Popup>
-    </Marker>
-    }
-  }
-  )
-  console.log("PINS:", breweryPins)
+      </Marker>
+      )
+  })
 
   return (
-    breweries && (
     <div className='map__container'>
-      <MapContainer center={['39.7614','-104.9839']} zoom={zoomLevel} scrollWheelZoom={false}>
+      <MapContainer key={mapKey} center={validBreweries && validBreweries.length !== 0 ? [validBreweries[0].latitude, validBreweries[0].longitude] : [39.82, -98.57]} zoom={validBreweries && validBreweries.length != 0 ? 10 :zoomLevel} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        {breweryPins}
-      
+        {mapPoints}
       </MapContainer>
-    </div> )) ||  null
-  
+    </div> )
 }
 
 export default Map;
