d17c90b53a6400739e22ed36ae365404a823f34c
Josh Bennett <joshbennett793@yahoo.com>
2023-09-15 10:37:27 -0500

    Update dispatch caller

--- src/Context/BreweryContext.js
+++ src/Context/BreweryContext.js
@@ -1,12 +1,34 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useReducer } from 'react';
 import { getBreweriesByCity, getBreweriesByState } from '../apiCalls';
+import PropTypes from 'prop-types';
 
 export const BreweryContext = createContext(null);
 
 export function BreweryContextProvider({ children }) {
+  const initialState = {
+    selectedBrewery: '',
+    isSelected: false
+  };
+
+  const breweryReducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_SELECTED_BREWERY':
+        return { ...state, selectedBrewery: action.id, isSelected: true };
+      case 'SET_IS_SELECTED':
+        return { ...state, isSelected: action.status };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(breweryReducer, initialState);
+
   const [breweries, setBreweries] = useState([]);
   const [noResults, setNoResults] = useState(false);
   const [error, setError] = useState('');
+
+  // const [selectedBrewery, setSelectedBrewery] = useState({});
+  // const [isSelected, setIsSelected] = useState(false)
 
   async function obtainBreweries(city, state) {
     let stateBreweryData = [];
@@ -18,8 +40,11 @@ export function BreweryContextProvider({ children }) {
         stateBreweryData = await getBreweriesByState(state);
 
         if (stateBreweryData.name !== 'Error') {
+          let onlyCoordsData = stateBreweryData.filter(
+            brewery => brewery.longitude && brewery.latitude
+          );
           setError(false);
-          setBreweries(stateBreweryData);
+          setBreweries(onlyCoordsData);
           setNoResults(false);
         }
         // Set error by default
@@ -37,7 +62,8 @@ export function BreweryContextProvider({ children }) {
 
         setError(false);
         filteredBreweryData = breweryData.filter(
-          brewery => brewery.state === state
+          brewery =>
+            brewery.state === state && brewery.latitude && brewery.longitude
         );
 
         if (filteredBreweryData.length === 0) {
@@ -50,19 +76,31 @@ export function BreweryContextProvider({ children }) {
     }
   }
 
+  // function setContextSelected(id){
+
+  // setSelectedBrewery(id);
+  // setIsSelected(true);
+  // }
+
+  const value = {
+    breweries,
+    obtainBreweries,
+    noResults,
+    setNoResults,
+    isSelected: state.isSelected,
+    selectedBrewery: state.selectedBrewery,
+    error,
+    setIsSelected: status => {
+      dispatch({ type: 'SET_IS_SELECTED', status });
+    },
+    setBreweries,
+    setContextSelected: id => {
+      dispatch({ type: 'SET_SELECTED_BREWERY', id });
+    }
+  };
+
   return (
-    <BreweryContext.Provider
-      value={{
-        breweries,
-        obtainBreweries,
-        noResults,
-        setNoResults,
-        error,
-        setError
-      }}
-    >
-      {children}
-    </BreweryContext.Provider>
+    <BreweryContext.Provider value={value}>{children}</BreweryContext.Provider>
   );
 }
 
