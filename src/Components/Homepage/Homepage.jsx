5662a8b99e7cc48eeb24dfd4c50ee6a3f83b4401
Renee Pinna <renee.pinna@gmail.com>
2023-09-14 19:00:53 -0600

    Resolve merge

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -2,19 +2,10 @@ import './Homepage.scss';
 
 import { useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import { useBreweries } from '../../Context/BreweryContext';
-
 import Map from '../Map/Map';
 import BreweryContainer from '../BreweryContainer/BreweryContainer';
 import Header from '../Header/Header';
-import Map from '../Map/Map';
-import Search from '../Search/Search';
-import { NavLink } from 'react-router-dom';
-import {useState} from 'react'
-import ErrorPage from '../ErrorPage/ErrorPage';
 import { useBreweries } from '../../Context/BreweryContext';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 function Homepage() {
   const { error } = useBreweries();
